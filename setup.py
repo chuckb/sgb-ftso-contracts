b72daeed32f6e258f59dd729efd12a1e858c5fcc
Chuck Benedict <chuck_benedict@yahoo.com>
2022-08-05 11:36:31 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
 # This call to setup() does all the work
 setup(
     name="sgb-ftso-contracts",
-    version="0.1.1",
+    version="0.1.2",
     description="Songbird network FTSO contract factory library",
     long_description=long_description,
     long_description_content_type="text/markdown",
